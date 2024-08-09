f3c7285cf68b1f530469d964813884c3dfd83764
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-08-09 11:40:21 +0900

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
+
 end
